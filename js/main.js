ff7db68afdc3fa31921abf54d25dea303692789d
Booldook <booldook@gmail.com>
2018-10-04 11:19:15 +0900

    181004-01

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(".searchs .hand").click(function(){
 	$(".search_catelist").stop().slideToggle(100);
 });
+
+var homes = new Array();
+var homes = [];
+homes[0] = [];
+homes[1] = [];
+homes[2] = [];
+homes[0][0] = "홍길동";
+homes[0][1] = 29;
+homes[0][2] = "M";
+homes[1][0] = "홍길순";
+homes[1][1] = 25;
+homes[1][2] = "F";
+homes[2][0] = "홍길만";
+homes[2][1] = 27;
+homes[2][2] = "M";
