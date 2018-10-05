a495e7aedc759d858d1d5a662fe3a5f9404a177b
Booldook <booldook@gmail.com>
2018-10-05 10:19:56 +0900

    181005-01

--- js/main.js
+++ js/main.js
@@ -2,17 +2,39 @@ $(".searchs .hand").click(function(){
 	$(".search_catelist").stop().slideToggle(100);
 });
 
-var homes = new Array();
-var homes = [];
-homes[0] = [];
-homes[1] = [];
-homes[2] = [];
-homes[0][0] = "홍길동";
-homes[0][1] = 29;
-homes[0][2] = "M";
-homes[1][0] = "홍길순";
-homes[1][1] = 25;
-homes[1][2] = "F";
-homes[2][0] = "홍길만";
-homes[2][1] = 27;
-homes[2][2] = "M";
+$(".menu li").hover(function(){
+	$(".menu_modal").stop().fadeOut(0);
+	$(this).children(".menu_modal").stop().fadeIn(100);
+}, function(){
+	$(".menu_modal").stop().fadeOut(0);
+});
+
+function modalMake0() {
+	var html = '';
+	var sites = [];
+	for(var i=0; i<10; i++) {
+		sites[i] = [];
+		sites[i][2] = '#';
+		sites[i][0] = '<li><img src="../img/main/site'+i+'.jpg" class="img" onclick="goUrl('+i+');"></li>';
+	}
+	sites[0][1] = '<li>Demo Default</li>';
+	sites[1][1] = '<li>Demo Decor</li>';
+	sites[2][1] = '<li>Demo Retail</li>';
+	sites[3][1] = '<li>Demo Books</li>';
+	sites[4][1] = '<li>Demo Fashion Color</li>';
+	sites[5][1] = '<li>Demo Lingerie</li>';
+	sites[6][1] = '<li>Demo Handmade</li>';
+	sites[7][1] = '<li>Demo Fashion</li>';
+	sites[8][1] = '<li>Demo Fashion Flat</li>';
+	sites[9][1] = '<li>Demo Electronics</li>';
+
+	for(i=0; i<sites.length; i++) {
+		html = '<ul>'+sites[i][0]+sites[i][1]+'</ul>';
+		$("#modal0").append(html);
+	}
+}
+modalMake0();
+
+function goUrl(n) {
+	location.href = sites[n][2];
+}
