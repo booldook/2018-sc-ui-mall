e51d326820dc3739ba80752535ff303a1d18a2f5
Booldook <booldook@gmail.com>
2018-10-11 13:20:16 +0900

    181011-06

--- js/main.js
+++ js/main.js
@@ -19,16 +19,16 @@ $.ajax({
 	type: "get",
 	dataType: "json",
 	data: {},
-	success: function(data){
+	success: function (data) {
 		var html;
-		for(var i=0; i<data.result.length; i++) {
+		for (var i = 0; i < data.result.length; i++) {
 			var html = '<ul>';
-			html += '<li><img src="'+data.result[i].img+'" class="img" onclick="goUrl(\''+data.result[i].link+'\');">'+data.result[i].title+'</li>';
+			html += '<li><img src="' + data.result[i].img + '" class="img" onclick="goUrl(\'' + data.result[i].link + '\');">' + data.result[i].title + '</li>';
 			html += '</ul>';
 			$("#modal0").append(html);
 		}
 	},
-	error: function(xhr, status, error) {
+	error: function (xhr, status, error) {
 		console.log(shr, status, error);
 	}
 });
@@ -39,31 +39,31 @@ $.ajax({
 	type: "get",
 	dataType: "json",
 	data: {},
-	success: function(data){
+	success: function (data) {
 		var cnt = data.result.length;
-		var style = 'style="width:'+(100/cnt+'%')+';"';
+		var style = 'style="width:' + (100 / cnt + '%') + ';"';
 		var html;
-		for(var i=0; i<cnt; i++) {
+		for (var i = 0; i < cnt; i++) {
 			//console.log(data.result[i].main.title);
-			html  = '<ul '+style+'>';
+			html = '<ul ' + style + '>';
 			html += '<li class="title">';
-			html += '<a href="'+data.result[i].main.link+'">';
+			html += '<a href="' + data.result[i].main.link + '">';
 			html += data.result[i].main.title;
 			html += '</a>';
-			html += '<div class="tooltip" style="background:'+data.result[i].main.color+'">';
+			html += '<div class="tooltip" style="background:' + data.result[i].main.color + '">';
 			html += data.result[i].main.icon;
-			html += '<div style="background:'+data.result[i].main.color+'"></div>';
+			html += '<div style="background:' + data.result[i].main.color + '"></div>';
 			html += '</div>';
 			html += '</li>';
-			for(var j=0; j<data.result[i].sub.length; j++) {
+			for (var j = 0; j < data.result[i].sub.length; j++) {
 				//console.log(data.result[i].sub[j].title);
 				html += '<li class="cont">';
-				html += '<a href="'+data.result[i].sub[j].link+'">';
+				html += '<a href="' + data.result[i].sub[j].link + '">';
 				html += data.result[i].sub[j].title;
 				html += '</a>';
-				html += '<div class="tooltip" style="background:'+data.result[i].sub[j].color+'">';
+				html += '<div class="tooltip" style="background:' + data.result[i].sub[j].color + '">';
 				html += data.result[i].sub[j].icon;
-				html += '<div style="background:'+data.result[i].sub[j].color+'"></div>';
+				html += '<div style="background:' + data.result[i].sub[j].color + '"></div>';
 				html += '</div>';
 				html += '</li>';
 			}
@@ -71,9 +71,9 @@ $.ajax({
 			$("#modal1").append(html);
 		}
 	},
-	error: function(xhr, status, error) {
+	error: function (xhr, status, error) {
 		console.log(xhr, status, error);
-	} 
+	}
 });
 
 /***** 카테고리 2 ******/
@@ -81,32 +81,41 @@ $.ajax({
 	url: "../json/cate2.json",
 	type: "get",
 	dataType: "json",
-	success: function(data) {
+	success: function (data) {
 		var html;
 		var blogs = data.result.blog;
 		var posts = data.result.recent;
 		//Blog 생성
-		for(var i=0; i<blogs.length; i++) {
-			html  = '<ul>';
+		for (var i = 0; i < blogs.length; i++) {
+			html = '<ul>';
 			html += '<li class="title">';
-			html += '<a href="'+blogs[i].main.link+'">'+blogs[i].main.title+'</a>';
+			html += '<a href="' + blogs[i].main.link + '">' + blogs[i].main.title + '</a>';
 			html += '</li>';
-			for(var j=0; j<blogs[i].sub.length; j++) {
+			for (var j = 0; j < blogs[i].sub.length; j++) {
 				html += '<li class="sub">';
-				html += '<a href="'+blogs[i].sub[j].link+'">'+blogs[i].sub[j].title+'</a>';
+				html += '<a href="' + blogs[i].sub[j].link + '">' + blogs[i].sub[j].title + '</a>';
 				html += '</li>';
 			}
 			html += '</ul>';
 			$("#modal2 > .blogs").append(html);
 		}
 		//Recent 생성
-		for(var i=0; i<posts.length; i++) {
+		for (var i = 0; i < posts.length; i++) {
 			html  = '<ul>';
+			html += '<li class="post clear" onclick="goPost(\''+posts[i].link+'\');">';
+			html += '<img src="'+posts[i].img+'" class="img post_img hover">';
+			html += '<div>';
+			html += '<div class="post_title">'+posts[i].title+'</div>';
+			html += '<span class="post_date">'+posts[i].date+'</span>';
+			html += '<span class="post_cnt">'+posts[i].comment+'</span>';
+			html += '<span class="post_comment">Comment</span>';
+			html += '</div>';
+			html += '</li>';
 			html += '</ul>';
 			$("#modal2 > .recents").append(html);
 		}
 	},
-	error: function(xhr, status, error) {
+	error: function (xhr, status, error) {
 		alert("통신이 원할하지 않습니다.\n잠시 후 다시 시도해 주세요.");
 		console.log(xhr, status, error);
 	}
