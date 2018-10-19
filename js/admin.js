b81c298b3d3c13e83032f9c631f6b1363ac40704
Booldook <booldook@gmail.com>
2018-10-19 14:19:48 +0900

    181019-04

--- js/admin.js
+++ js/admin.js
@@ -13,18 +13,56 @@ var ref;
 var key;
 
 /***** HOME ******/
+
+(function initHome() {
+	$(".list:not(#home_wr)").remove();
+	ref = db.ref("root/home");
+	ref.on("child_added", homeAdd);
+})();
+function homeAdd(data) {
+	var id = data.key;
+	var img = data.val().img;
+	var src = '../img/main/'+img;
+	var title = data.val().title;
+	var link = data.val().link;
+	var html = '';
+	html += '<ul class="list clear row" id="'+id+'">';
+	html += '<li class="col-xs-4 col-sm-3 col-md-2 col-lg-2">';
+	html += '<div>';
+	html += '<img src="'+src+'">';
+	html += '<input type="text" class="tit_img form-control" placeholder="이미지" value="'+img+'">';
+	html += '</li>';
+	html += '<li class="col-xs-4 col-sm-6 col-md-7 col-lg-8">';
+	html += '<div>';
+	html += '<input type="text" class="title form-control" placeholder="타이틀" value="'+title+'">';
+	html += '<input type="text" class="link form-control" style="margin-top:5px;" placeholder="링크주소" value="'+link+'">';
+	html += '</div>';
+	html += '</li>';
+	html += '<li class="col-xs-4 col-sm-3 col-md-3 col-lg-2">';
+	html += '<div>';
+	html += '<button class="btn btn-danger" onclick="homeDel(this);">삭제</button>';
+	html += '<button class="btn btn-warning" onclick="homeUp(this);">수정</button>';
+	html += '</div>';
+	html += '</li>';
+	html += '</ul>';
+	$("#home_wrap").append(html);
+}
+
 $("#home_save").on('click', function(){
+	var img = $("#home_wr .tit_img").val();
 	var title = $("#home_wr .title").val();
 	var link = $("#home_wr .link").val();
-	if(title == '' || link == '') {
+	if(title == '' || link == '' || img == '') {
 		alert("내용을 적어주세요.");
 	}
 	else {
 		ref = db.ref("root/home");
 		ref.push({
+			img: img,
 			title: title,
 			link: link
 		}).key;
+		alert("등록되었습니다.");
 	}
 });
 
