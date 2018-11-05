5e120dedf3203eef6b0a32fe917fe009a8a27326
Booldook <booldook@gmail.com>
2018-11-05 11:18:48 +0900

    181005-02

--- js/main.js
+++ js/main.js
@@ -262,36 +262,17 @@ $("footer > div").click(function(){
 });
 */
 
-
-var Ajax = (function(){
-	function Ajax(_file) {
-		this.file = _file;
-		this.data = {};
-	}
-	Ajax.prototype.addData = function(_data) {
-		this.data = _data;
-	}
-	Ajax.prototype.send = function(_fn) {
-		this.fn = _fn;
-		$.ajax({
-			url: this.file,
-			type: "post",
-			dataType: "json",
-			data: this.data,
-			success: this.fn,
-			error: function (xhr, status, error) {
-				alert("통신이 원할하지 않습니다.\n잠시 후 다시 시도해 주세요.");
-				console.log(xhr, status, error);
-			}
-		});
-	}
-	return Ajax;
-}());
-
+/***** 왼쪽 카테고리 생성 *****/
 var sFn = function(data) {
 	if(data.result) {
-		for(var i=0; i<data.result.cates.length; i++) {
-			console.log(data.result.cates[i]);
+		for(var i=0, html='', rs; i<data.result.cates.length; i++) {
+			rs = data.result.cates[i];
+			html = '<li>';
+			html+= '<span class="'+rs.icon+'"></span>';
+			html+= '<a href="'+rs.link+'"><span>'+rs.title+'</span></a>';
+			if(rs.ajax != '') html += '<span class="fas fa-angle-right"></span>';
+			html+= '</li>'
+			$(".banners .cate").append(html);
 		}
 	}
 }
