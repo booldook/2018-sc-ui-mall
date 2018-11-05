1a881de99c973dba30a49b91ab89c7c870a68492
Booldook <booldook@gmail.com>
2018-11-05 10:42:26 +0900

    ...

---
+++ js/Ajax.js
@@ -0,0 +1,24 @@
+var Ajax = (function(){
+	function Ajax(_file) {
+		this.file = _file;
+		this.data = {};
+	}
+	Ajax.prototype.addData = function(_data) {
+		this.data = _data;
+	}
+	Ajax.prototype.send = function(_fn) {
+		this.fn = _fn;
+		$.ajax({
+			url: this.file,
+			type: "post",
+			dataType: "json",
+			data: this.data,
+			success: this.fn,
+			error: function (xhr, status, error) {
+				alert("통신이 원할하지 않습니다.\n잠시 후 다시 시도해 주세요.");
+				console.log(xhr, status, error);
+			}
+		});
+	}
+	return Ajax;
+}());
