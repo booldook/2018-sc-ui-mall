8e02238cb0e23316bbc543cb1ca2697683ce2aea
Booldook <booldook@gmail.com>
2018-11-06 11:22:17 +0900

    181006-01

---
+++ js/SpaAni.js
@@ -0,0 +1,30 @@
+var SpaAni = (function(){
+	function SpaAni(_page, _elem, _gap) {
+		var obj = this;
+		this.page = $(_page);
+		this.elem = _elem;
+		this.scTop = 0;
+		this.pos = [];
+		this.now = 0;
+		this.gap = 0;
+		$(window).resize(function(){
+			for(var i=0; i<obj.page.length; i++) {
+				obj.pos[i] = $(obj.page[i]).position().top;
+			}
+		}).trigger("resize");
+		$(window).scroll(function(){
+			obj.scTop = $(this).scrollTop();
+			obj.init(obj);
+		}).trigger("scroll");
+	};
+	SpaAni.prototype.init = function(obj){
+		for(var i=0; i<obj.page.length; i++) {
+			if(obj.scTop+obj.gap > obj.pos[i]) obj.now = i;
+		}
+		$(obj.page[obj.now]).find(obj.elem).each(function(){
+			var cls = $(this).data("ani");
+			$(this).addClass(cls);
+		});
+	};
+	return SpaAni;
+}());
---
+++ js/ani_sample.js
@@ -0,0 +1,1 @@
+var pages = new SpaAni(".page", ".ani", 500);
