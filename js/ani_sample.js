aa85e70e9b5134d6cef509b15469d2339e624ba5
Booldook <booldook@gmail.com>
2018-11-07 11:08:18 +0900

    181107-01

--- js/ani_sample.js
+++ js/ani_sample.js
@@ -1,1 +1,13 @@
 var pages = new SpaAni(".section", ".ani", 300);
+
+$(".section").eq(3).mousemove(function(evt){
+	var delta = 30;
+	var cX = evt.clientX;
+	var cY = evt.clientY;
+	var iX = $(this).find(".chair").width()/2;
+	var iY = $(this).find(".chair").height()/2;
+	var mX = (iX - cX)/delta;
+	var mY = (iY - cY)/delta;
+	console.log(mX, mY);
+	$(this).find(".chair").css("transform", "translate("+mX+"px, "+mY+"px)");
+});
